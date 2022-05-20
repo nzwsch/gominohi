c2d01dd9ad01d5d63ca45eb3d8797b292549be76
Seiichi Yonezawa <nzwsch@gmail.com>
2022-05-21 00:36:51 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ garvage_collection_csv  = CSV.parse(garvage_collection_body, headers: true)
 row = garvage_collection_csv.first
 
 collection_areas = row.headers[2..]
-p collection_areas.size
+original_collection_areas = collection_areas.size
 collection_areas.each { |name| CollectionArea.create_by_area_name(name) }
 
 collection_dates = []
@@ -74,6 +74,52 @@ grouped_dates.each do |name, collection_dates|
   CollectionDate.insert_all(collection_dates)
 end
 
-# garvage_collection_path = Rails.root.join("db/garvagecollectioncalendar201910.csv")
+garvage_collection_path = Rails.root.join("db/garvagecollectioncalendar201910.csv")
+garvage_collection_body = File.read(garvage_collection_path, encoding: 'bom|utf-8')
+garvage_collection_csv  = CSV.parse(garvage_collection_body, headers: true)
+
+row = garvage_collection_csv.first
+
+collection_areas = row.headers[2..]
+warn "[!] collection_areas has changed" if collection_areas.size != original_collection_areas
+
+collection_dates = []
+
+garvage_collection_csv.each do |row|
+  date    = row[0]
+  weekday = row[1]
+
+  row.headers[2..].each_with_index do |name, index|
+    gomi_type_id = row[2 + index]
+
+    next if gomi_type_id.to_i == 0
+
+    collection_dates.push({
+      date: date,
+      weekday: weekday,
+      name: name,
+      gomi_type_id: gomi_type_id
+    })
+  end
+end
+
+grouped_dates = collection_dates.group_by { |item| item[:name] }
+
+grouped_dates.each do |name, collection_dates|
+  collection_area_id = CollectionArea.find_by_name(name).id
+  collection_dates   = collection_dates.map do |item|
+    {
+      date: item[:date],
+      weekday: item[:weekday],
+      collection_area_id: collection_area_id,
+      gomi_type_id: item[:gomi_type_id],
+      created_at: Time.current,
+      updated_at: Time.current,
+    }
+  end
+
+  CollectionDate.insert_all(collection_dates)
+end
+
 # garvage_collection_path = Rails.root.join("db/garvagecollectioncalendar202010.csv")
 # garvage_collection_path = Rails.root.join("db/garvagecollectioncalendar202110.csv")

1d17ff51b90c7f37ca0e31703a74840aa0be2d15
Seiichi Yonezawa <nzwsch@gmail.com>
2022-05-21 01:31:18 +0900

    Rename variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ gomitype_csv.each do |row|
 end
 
 # Import csv
-class ImportCsvTask
+module SeedTask
   AREAS = %w[
     中央区
     豊平区
@@ -28,103 +28,105 @@ class ImportCsvTask
     手稲区
   ].freeze
 
-  def initialize(csv_file_path)
-    csv_path = Rails.root.join(csv_file_path)
-    csv_body = File.read(csv_path, encoding: 'bom|utf-8')
-    @csv_data = CSV.parse(csv_body, headers: true)
-  end
-
-  def collection_areas
-    @csv_data.first.headers[2..]
-  end
-
-  # C: Metrics/MethodLength: Method has too many lines. [15/10]
-  def import!
-    grouped_dates = filter_collection_dates.group_by { |item| item[:name] }
-
-    grouped_dates.each do |name, collection_dates|
-      collection_area_id = CollectionArea.find_by_name(name).id
-      collection_dates   = collection_dates.map do |item|
-        {
-          date: item[:date],
-          weekday: item[:weekday],
-          collection_area_id: collection_area_id,
-          gomi_type_id: item[:gomi_type_id],
-          created_at: Time.current,
-          updated_at: Time.current
-        }
-      end
-
-      CollectionDate.insert_all(collection_dates)
+  class CsvImporter
+    def initialize(csv_file_path)
+      csv_path = Rails.root.join(csv_file_path)
+      csv_body = File.read(csv_path, encoding: 'bom|utf-8')
+      @csv_data = CSV.parse(csv_body, headers: true)
     end
-  end
 
-  private
+    def collection_areas
+      @csv_data.first.headers[2..]
+    end
 
-  # C: Metrics/MethodLength: Method has too many lines. [16/10]
-  def filter_collection_dates
-    collection_dates = []
+    # C: Metrics/MethodLength: Method has too many lines. [15/10]
+    def import!
+      grouped_dates = filter_collection_dates.group_by { |item| item[:name] }
 
-    @csv_data.each do |row|
-      date    = row[0]
-      weekday = row[1]
+      grouped_dates.each do |name, collection_dates|
+        collection_area_id = CollectionArea.find_by_name(name).id
+        collection_dates   = collection_dates.map do |item|
+          {
+            date: item[:date],
+            weekday: item[:weekday],
+            collection_area_id: collection_area_id,
+            gomi_type_id: item[:gomi_type_id],
+            created_at: Time.current,
+            updated_at: Time.current
+          }
+        end
 
-      row.headers[2..].each_with_index do |name, index|
-        gomi_type_id = row[2 + index]
-
-        next if gomi_type_id.to_i.zero?
-
-        collection_dates.push(
-          date: date,
-          weekday: weekday,
-          name: replace_special_numbers(name),
-          gomi_type_id: gomi_type_id
-        )
+        CollectionDate.insert_all(collection_dates)
       end
     end
 
-    collection_dates
-  end
+    private
 
-  def replace_special_numbers(name)
-    name
-      .gsub('①', '1')
-      .gsub('②', '2')
-      .gsub('③', '3')
-      .gsub('④', '4')
-      .gsub('⑤', '5')
-      .gsub('⑥', '6')
-      .gsub('⑦', '7')
+    # C: Metrics/MethodLength: Method has too many lines. [16/10]
+    def filter_collection_dates
+      collection_dates = []
+
+      @csv_data.each do |row|
+        date    = row[0]
+        weekday = row[1]
+
+        row.headers[2..].each_with_index do |name, index|
+          gomi_type_id = row[2 + index]
+
+          next if gomi_type_id.to_i.zero?
+
+          collection_dates.push(
+            date: date,
+            weekday: weekday,
+            name: replace_special_numbers(name),
+            gomi_type_id: gomi_type_id
+          )
+        end
+      end
+
+      collection_dates
+    end
+
+    def replace_special_numbers(name)
+      name
+        .gsub('①', '1')
+        .gsub('②', '2')
+        .gsub('③', '3')
+        .gsub('④', '4')
+        .gsub('⑤', '5')
+        .gsub('⑥', '6')
+        .gsub('⑦', '7')
+    end
   end
 end
 
-ImportCsvTask::AREAS.each { |name| Area.create(name: name) }
+SeedTask::AREAS.each { |name| Area.create(name: name) }
 
-import_csv_task = ImportCsvTask.new('db/2017100120191008garvagecollectioncalendar.csv')
+csv_importer = SeedTask::CsvImporter.new('db/2017100120191008garvagecollectioncalendar.csv')
 
-collection_areas = import_csv_task.collection_areas
-original_collection_areas = collection_areas.size
+collection_areas = csv_importer.collection_areas
+original_size = collection_areas.size
 collection_areas.each { |name| CollectionArea.create_by_area_name(name) }
 
-import_csv_task.import!
+csv_importer.import!
 
-import_csv_task = ImportCsvTask.new('db/garvagecollectioncalendar201910.csv')
+csv_importer = SeedTask::CsvImporter.new('db/garvagecollectioncalendar201910.csv')
 
-collection_areas = import_csv_task.collection_areas
-warn '[!] collection_areas has changed' if collection_areas.size != original_collection_areas
+current_size = csv_importer.collection_areas.size
+warn '[!] collection_areas has changed' if current_size != original_size
 
-import_csv_task.import!
+csv_importer.import!
 
-import_csv_task = ImportCsvTask.new('db/garvagecollectioncalendar202010.csv')
+csv_importer = SeedTask::CsvImporter.new('db/garvagecollectioncalendar202010.csv')
 
-collection_areas = import_csv_task.collection_areas
-warn '[!] collection_areas has changed' if collection_areas.size != original_collection_areas
+current_size = csv_importer.collection_areas.size
+warn '[!] collection_areas has changed' if current_size != original_size
 
-import_csv_task.import!
+csv_importer.import!
 
-import_csv_task = ImportCsvTask.new('db/garvagecollectioncalendar202110.csv')
+csv_importer = SeedTask::CsvImporter.new('db/garvagecollectioncalendar202110.csv')
 
-collection_areas = import_csv_task.collection_areas
-warn '[!] collection_areas has changed' if collection_areas.size != original_collection_areas
+current_size = csv_importer.collection_areas.size
+warn '[!] collection_areas has changed' if current_size != original_size
 
-import_csv_task.import!
+csv_importer.import!

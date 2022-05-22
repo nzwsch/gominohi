19b892f20d242cb87e083326b90a169e2b8df935
Seiichi Yonezawa <nzwsch@gmail.com>
2022-05-22 16:58:31 +0900

    Update calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,3 +1,17 @@
 const calendar = document.getElementById('calendar')
+const wrapper = calendar.querySelector('.wrapper')
 
-console.log('hello')
+const WEEKDAYS=['日', '月', '火', '水', '木', '金', '土']
+
+for (let i = 0; i < 7; i++) {
+  const div = document.createElement('div')
+  div.classList.add("week")
+  div.innerHTML = WEEKDAYS[i]
+  wrapper.appendChild(div)
+}
+
+for (let i = 0; i < 7*5; i++) {
+  const div = document.createElement('div')
+  div.classList.add("date")
+  wrapper.appendChild(div)
+}
